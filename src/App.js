e82001b685cbda79c6746f0de13205bbb26c9510
Joffrey LAGUT <j.lagut@live.fr>
2018-10-13 00:11:59 +0200

    Mockup & file cleaning. - Replace the app with a mockup of what i want to do. - Remove the files not used.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div style={{padding:'10px'}}>
+        <header>
+          <h1>Joffrey's library</h1>
+          <div>
+            <input type="text" placeholder="Search" />
+            <span style={{'margin-left':'10px'}}>12 items</span>
+          </div>
         </header>
+        <hr />
+        <div className="itemList">
+          <div className="item">Item 1</div>       
+          <div className="item">Item 2</div>       
+          <div className="item">Item 3</div>       
+          <div className="item">Item 4</div>       
+          <div className="item">Item 5</div>   
+          <div className="item">Item 6</div>       
+          <div className="item">Item 7</div>       
+          <div className="item">Item 8</div>       
+          <div className="item">Item 9</div>       
+          <div className="item">Item 10</div>   
+          <div className="item">Item 11</div>       
+          <div className="item">Item 12</div>       
+        </div>
       </div>
     );
   }
