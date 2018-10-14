47a1dcb6b5cec6372c583bb8c482618bcd890135
Joffrey LAGUT <j.lagut@live.fr>
2018-10-14 22:26:12 +0200

    Add fake data and simulate some latency before we get the data and a functional search field. - Add a fake object representing what we will receive from the server. - Delay the setState 1 second after the component did mount. - Add conditions in the code to show a message if we don't have data yet. - Replace the static item by the one we have.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let fakeServerData = {
+  'name': 'Joffrey',
+  'items':[
+    {
+      'name':'Halo 5',
+      'platform':'Xbox One'
+    },
+    {
+      'name':'Doom',
+      'platform':'PC'
+    }
+  ]
+}
+
+
 class App extends Component {
+
+  componentDidMount(){
+    // Simulation of the call to a remote server returning a value 1 second
+    // after the componend did mount.
+    let data = Object.assign({}, fakeServerData);
+    setTimeout(()=>{
+      this.setState({
+        data
+      })
+    }, 1000);
+  }
+
   render() {
     return (
-      <div style={{padding:'10px'}}>
-        <header>
-          <h1>Joffrey's library</h1>
+      <div>
+        {this.state && this.state.data ? (
           <div>
-            <input type="text" placeholder="Search" />
-            <span style={{'margin-left':'10px'}}>12 items</span>
+            <header>
+              <h1>{this.state.data.name}'s library</h1>
+              <div>
+                <input type="text" placeholder="Search" onChange={(e) => {
+                  let stringToSearch = e.target.value;
+                  let newState = Object.assign({}, this.state);
+                  newState.data.items = fakeServerData.items.filter((item) => {
+                    return item.name.toLowerCase().includes(stringToSearch)
+                  })
+                  this.setState(newState)} 
+                  }
+                />
+                <span style={{'margin-left':'10px'}}>{this.state.data.items.length} items</span>
+              </div>
+            </header>
+            <hr />
+            <div className="itemList">
+              {this.state.data.items ? this.state.data.items.map((item, i) => 
+                <div className="item" key={i}>{item.name}</div> 
+              ):''}      
+            </div>
           </div>
-        </header>
-        <hr />
-        <div className="itemList">
-          <div className="item">Item 1</div>       
-          <div className="item">Item 2</div>       
-          <div className="item">Item 3</div>       
-          <div className="item">Item 4</div>       
-          <div className="item">Item 5</div>   
-          <div className="item">Item 6</div>       
-          <div className="item">Item 7</div>       
-          <div className="item">Item 8</div>       
-          <div className="item">Item 9</div>       
-          <div className="item">Item 10</div>   
-          <div className="item">Item 11</div>       
-          <div className="item">Item 12</div>       
-        </div>
+        ) : (
+          <div><span>Loading in progress...</span></div>
+        )}
       </div>
     );
   }
